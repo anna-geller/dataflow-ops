8db2e562930d5f8115a9515854e6d8ad7732df4e
Anna Geller <anna@prefect.io>
2022-07-08 09:09:09 +0200

    ccc

--- flows/healthcheck.py
+++ flows/healthcheck.py
@@ -3,7 +3,7 @@ from prefect import task, flow
 from prefect import get_run_logger
 from prefect.flow_runners import UniversalFlowRunner
 from prefect.deployments import DeploymentSpec
-# from prefect.deployments import Deployment  # todo  xxxxxx yyynnnn
+# from prefect.deployments import Deployment  # todo  xxxxxx yyynnnnccc
 
 
 @task
