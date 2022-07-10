3ca94e2b9880c447384521c44d909834fb8b665d
Anna Geller <anna@prefect.io>
2022-07-10 03:22:57 +0200

    retrigger build + agent

--- prefect_dataops/postgres_utils.py
+++ prefect_dataops/postgres_utils.py
@@ -1,14 +1,14 @@
-# import pandas as pd
-# import sqlalchemy
-#
-#
-# def get_db_connection_string(
-#     user: str, password: str, host: str = "localhost", db: str = "postgres"
-# ) -> str:
-#     return f"postgresql://{user}:{password}@{host}:5432/{db}"
-#
-#
-# def get_df_from_sql_query(table_or_query: str) -> pd.DataFrame:
-#     db = get_db_connection_string()
-#     engine = sqlalchemy.create_engine(db)
-#     return pd.read_sql(table_or_query, engine)
+import pandas as pd
+import sqlalchemy
+
+
+def get_db_connection_string(
+    user: str, password: str, host: str = "localhost", db: str = "postgres"
+) -> str:
+    return f"postgresql://{user}:{password}@{host}:5432/{db}"
+
+
+def get_df_from_sql_query(table_or_query: str) -> pd.DataFrame:
+    db = get_db_connection_string()
+    engine = sqlalchemy.create_engine(db)
+    return pd.read_sql(table_or_query, engine)
