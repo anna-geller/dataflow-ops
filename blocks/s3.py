ca0e5be78598b2cf96d3ac0d38a7b975d3f3da35
Anna Geller <anna@prefect.io>
2022-09-13 13:18:38 +0200

    cw logs

--- blocks/s3.py
+++ blocks/s3.py
@@ -3,6 +3,6 @@ from prefect.filesystems import S3
 s3 = S3(
     bucket_path="prefect-orion/prod",
     aws_access_key_id="xxx",  # when creating a block, you can pass this value from CI/CD Secrets
-    aws_secret_access_key="xxx",  # when creating a block, you can pass this value from CI/CD Secrets
+    aws_secret_access_key="xxx",  # or retrieve those from environment variables
 )
 s3.save("prod", overwrite=True)
---
+++ infrastructure/remove_all_flows.py
@@ -0,0 +1,16 @@
+import asyncio
+from prefect.client import get_client
+
+
+async def remove_all_flows():
+    orion_client = get_client()
+    flows = await orion_client.read_flows()
+    for flow in flows:
+        flow_id = flow.id
+        print(f"Deleting flow: {flow.name}, {flow_id}")
+        await orion_client._client.delete(f"/flows/{flow_id}")
+        print(f"Flow with UUID {flow_id} deleted")
+
+
+if __name__ == "__main__":
+    asyncio.run(remove_all_flows())
